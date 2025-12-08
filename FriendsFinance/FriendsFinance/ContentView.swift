1b3d0194c4668882befefd8ab55a20c83d10ad67
fahamed1 <fahamed1@fordham.edu>
2025-12-07 20:09:21 -0500

    Initial commit

---
+++ FriendsFinance/FriendsFinance/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FriendsFinance
+//
+//  Created by Farhat Ahamed on 12/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FriendsFinance/FriendsFinance/FriendsFinanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsFinanceApp.swift
+//  FriendsFinance
+//
+//  Created by Farhat Ahamed on 12/7/25.
+//
+
+import SwiftUI
+
+@main
+struct FriendsFinanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
